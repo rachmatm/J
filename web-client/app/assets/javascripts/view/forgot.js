3487f8219e9fe3659fd52753535db58516e4f943
dicky <dicky.hendarsah@gmail.com>
2012-03-15 17:17:43 +0700

    fix forgot

--- web-client/app/assets/javascripts/view/forgot.js
+++ web-client/app/assets/javascripts/view/forgot.js
@@ -15,16 +15,55 @@ window.ForgotView = Backbone.View.extend({
     this.formView.setElement('#main-forgot-form');
 
     this.formView.render({
-         rules: {},
+         rules: {
+             'forgot[email]': {
+          required: true,
+          email: true
+        }
+         },
          messages: {},
-         errorPlacement: function(error, element){},
-         success: function(label){},
-         highlight: function(element, errorClass, validClass){},
-         unhighlight: function(element, errorClass, validClass){},
+         errorPlacement: function(error, element){
+              var $alert_section = element.parents('tr').children('.main-forgot-form-field-alert');
+                $alert_section.children('.error').html(error).removeClass('hidden');
+                $alert_section.children('.default_text').addClass('hidden');
+         },
+         success: function(label){
+             var $alert_section = label.parents('.main-forgot-form-field-alert');
+            $alert_section.children('.error').addClass('hidden');
+            $alert_section.children('.default_text').removeClass('hidden');
+         },
+         highlight: function(element, errorClass, validClass){
+              var $el = $(element);
+                $el.addClass(errorClass).removeClass(validClass);
+                var $alert_section = $el.parents('tr').children('.main-forgot-form-field-alert');
+                $alert_section.children('.error').removeClass('hidden');
+                $alert_section.children('.default_text').addClass('hidden');
+         },
+         unhighlight: function(element, errorClass, validClass){
+             var $el = $(element);
+        $el.removeClass(errorClass).addClass(validClass);
+
+        var $alert_section = $el.parents('tr').children('.main-forgot-form-field-alert');
+
+        $alert_section.children('.error').addClass('hidden');
+        $alert_section.children('.default_text').removeClass('hidden');
+         },
          submitHandler: function(form){
             $(form).ajaxSubmit({
                 dataType: 'json',
-                success: function(data, textStatus, jqXHR){},
+                success: function(data, textStatus, jqXHR){
+                    alert(data);
+                    if(data.failed === true){
+              _this.alertView.remove();
+              _this.alertView.render('forgot-alert',{error: data.error, errors: data.errors});
+              _this.recaptchaView.reload();
+            }
+            else{
+              _this.alertView.remove();
+              _this.alertView.render({notice: data.notice});
+              location.href = '/';
+            }
+                },
                 error: function(jqXHR, textStatus, errorThrown){
                     alert(textStatus);
                 }
