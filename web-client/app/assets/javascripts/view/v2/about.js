e0e2a12207890b492da56c3df32e9b23b6378d67
dicky <dicky.hendarsah@gmail.com>
2012-03-20 10:38:34 +0700

    about+forgot

--- web-client/app/assets/javascripts/view/v2/about.js
+++ web-client/app/assets/javascripts/view/v2/about.js
@@ -1,8 +1,15 @@
+/*
 window.AboutView = Backbone.View.extend({
     
   template: _.template($('#main-about-template').html()),
 
   render: function(){
+    
     $(this.el).html(this.template);
   }
+});
+*/
+
+window.AboutView = AppView.extend({
+  template: _.template($('#main-about-template').html())
 });
