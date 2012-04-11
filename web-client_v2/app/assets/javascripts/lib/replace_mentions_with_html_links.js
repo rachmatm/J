2c37fab90a853514409a33608810b7bc74f1399e
bimozx <bimozx@gmail.com>
2012-04-11 16:21:19 +0700

    applying boundary fix

--- web-client_v2/app/assets/javascripts/lib/replace_mentions_with_html_links.js
+++ web-client_v2/app/assets/javascripts/lib/replace_mentions_with_html_links.js
@@ -3,9 +3,10 @@ function replaceMentionsWithHtmlLinks(text, mentions) {
   var modified_text = text;
 
   _.each(mentioned_names, function(data) {
-    var regexp = new RegExp(data, 'gi');
+    var regexp = new RegExp(data + '\\b', 'gi');
     var sliced_name = data.slice(1, data.length);
 
+    console.log(regexp);
     if (in_array(sliced_name, _.pluck(mentions, 'username'))) {
       var user = _.find(mentions, function(hash) {
         return hash.username == sliced_name;
