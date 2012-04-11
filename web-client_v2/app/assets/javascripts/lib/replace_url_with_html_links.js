846b667c1d3ea68c0c9c90f7084f4b7dcd2cb0ed
Rachmat Maulana <maulana.rachmat@gmail.com>
2012-04-11 13:23:47 +0700

    Links harus bisa diklik (open in a new window)

--- web-client_v2/app/assets/javascripts/lib/replace_url_with_html_links.js
+++ web-client_v2/app/assets/javascripts/lib/replace_url_with_html_links.js
@@ -1,6 +1,7 @@
 //http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
 
-function replaceURLWithHTMLLinks(text) {
+function replaceURLWithHTMLLinks(text, target) {
+    var target = '_self' || target;
     var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(exp,"<a href='$1'>$1</a>");
+    return text.replace(exp,"<a href='$1' target='"+ target +"'>$1</a>");
 }
