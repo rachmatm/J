4b92df8a78d79bbae1846da4fe556b2872951978
Rachmat Maulana <maulana.rachmat@gmail.com>
2012-04-11 13:19:18 +0700

    Avatar harus sesuai ama profil , Body jot dibolehin ada sampai dua line break berturut2. Links harus bisa diklik (open in a new window)

---
+++ web-client_v2/app/assets/javascripts/lib/replace_url_with_html_links.js
@@ -0,0 +1,6 @@
+//http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
+
+function replaceURLWithHTMLLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp,"<a href='$1'>$1</a>");
+}
