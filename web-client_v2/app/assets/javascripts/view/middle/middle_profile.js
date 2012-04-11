257ab4caadddbeacc0f2b6e043dd8f93d6dd6058
bimozx <bimozx@gmail.com>
2012-04-11 20:22:51 +0700

    Finishing the method for submitting the changes

--- web-client_v2/app/assets/javascripts/view/middle/middle_profile.js
+++ web-client_v2/app/assets/javascripts/view/middle/middle_profile.js
@@ -5,6 +5,7 @@ window.MiddleProfileView = Backbone.View.extend({
   initialize: function(){
     this.profiles = new ProfileCollection;
     this.profiles.bind('reset', this.setData, this);
+    this.profiles.bind('change', this.setTemplate, this);
   },
 
   render: function(){
@@ -19,7 +20,135 @@ window.MiddleProfileView = Backbone.View.extend({
     });
   },
 
+  showAllField: function(){
+    $('.pp_nickname').removeClass('hidden');
+    $('.pp_realname').removeClass('hidden');
+    $('.pp_header_middle p').removeClass('hidden');
+    $('.pp_header_middle a').removeClass('hidden');
+  },
+
+  hideAllField: function(){
+    $('.pp_nickname').addClass('hidden');
+    $('.pp_realname').addClass('hidden');
+    $('.pp_header_middle p').addClass('hidden');
+    $('.pp_header_middle a').addClass('hidden');
+  },
+
   setTemplate: function(data){
+    var _this = this;
+
     $(this.el).html(this.template(data.toJSON()));
+
+    $('.pp_send_message').toggle(function(){
+      $('.show-hide-edit-panel-input').removeClass('hidden');
+      _this.hideAllField();
+    }, function(){
+      $('.show-hide-edit-panel-input').addClass('hidden');
+      _this.showAllField();
+    });
+
+    $('#profile-form-username').validate({
+      rules: {
+        'profile[username]': {
+          required: true,
+          username: true
+        }
+      },
+      submitHandler: function(form){
+
+        $(form).ajaxSubmit({
+          error: function(jqXHR, textStatus, errorThrown){
+            _this.submitError.call(jqXHR, textStatus, errorThrown);
+          },
+          success: function(xhrData, textStatus, jqXHR){
+            _this.submitSuccess.call(_this, xhrData, textStatus, jqXHR, data);
+          }
+        });
+        return false;
+      }
+    });
+
+    $('#profile-form-realname').validate({
+      rules: {
+        'profile[realname]': {
+          required: true
+        }
+      },
+      submitHandler: function(form){
+
+        $(form).ajaxSubmit({
+          error: function(jqXHR, textStatus, errorThrown){
+            _this.submitError.call(jqXHR, textStatus, errorThrown);
+          },
+          success: function(xhrData, textStatus, jqXHR){
+            _this.submitSuccess.call(_this, xhrData, textStatus, jqXHR, data);
+          }
+        });
+        return false;
+      }
+    });
+
+    $('#profile-form-bio').validate({
+      rules: {
+        'profile[bio]': {
+          required: true
+        }
+      },
+      submitHandler: function(form){
+
+        $(form).ajaxSubmit({
+          error: function(jqXHR, textStatus, errorThrown){
+            _this.submitError.call(jqXHR, textStatus, errorThrown);
+          },
+          success: function(xhrData, textStatus, jqXHR){
+            _this.submitSuccess.call(_this, xhrData, textStatus, jqXHR, data);
+          }
+        });
+        return false;
+      }
+    });
+
+    $('#profile-form-url').validate({
+      rules: {
+        'profile[url]': {
+          required: true,
+          url: true
+        }
+      },
+      submitHandler: function(form){
+
+        $(form).ajaxSubmit({
+          error: function(jqXHR, textStatus, errorThrown){
+            _this.submitError.call(jqXHR, textStatus, errorThrown);
+          },
+          success: function(xhrData, textStatus, jqXHR){
+            _this.submitSuccess.call(_this, xhrData, textStatus, jqXHR, data);
+          }
+        });
+        return false;
+      }
+    });
+
+  },
+
+  submitError: function(jqXHR, textStatus, errorThrown){
+    alert(textStatus);
+  },
+
+  submitSuccess: function(xhrData, textStatus, jqXHR, model){
+    var _this = this;
+    if(xhrData.failed === true){
+      alert(xhrData.error);
+    }
+    else{
+      if(xhrData.error === true){
+        alert(xhrData.error);
+      }
+      else{
+        model.set($.extend(xhrData.content, {
+          i: Date.now()
+          }));
+      }
+    }
   }
 });
